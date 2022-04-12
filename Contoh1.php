842fd96adbef63f7d3fb5b6044baeca4761e6ae9
saferius <103491341+saferius@users.noreply.github.com>
2022-04-12 15:56:30 +0700

    Delete Contoh1.php

--- Contoh1.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Contoh1 extends CI_Controller{
-
-    public function index(){
-        echo "<h1>Perkenalkan</h1>";
-        echo"Nama saya Saferius Gowasa saya tinggal di daerah pasar baru jakarta pusat, hoby saya main fursal";
-    }
-}
